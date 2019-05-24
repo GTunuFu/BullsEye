81d51ed72d3d65fbf7b84a125d456a8bacbab8e3
Geeth Tunuguntla <34226112+GTunuFu@users.noreply.github.com>
2019-05-23 21:46:22 -0500

    Add files via upload

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Geeth Tunuguntla on 5/23/19.
+//  Copyright © 2019 GeeFu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello, World",
+                                      message: "This is my first app!",
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler:nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
